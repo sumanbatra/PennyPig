523015549820cfe7b73cb5004645e13d31529905
Ritu kaly <kalsyitu.rk@gmail.com>
2019-10-28 19:56:38 -0400

    main activity 2

--- app/src/main/java/com/example/pennypig/Main2Activity.java
+++ app/src/main/java/com/example/pennypig/Main2Activity.java
@@ -3,8 +3,16 @@ package com.example.pennypig;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+
+    public void onclick(View view){
+        String tag = view.getTag().toString();
+        Toast.makeText(getApplicationContext(),tag,Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
