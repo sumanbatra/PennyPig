88da21ec2f9a84f9b9fc830d0a096c3fe3b5c73d
Ram Keerthy Athinarayanan <rkeerthya@gmail.com>
2019-10-28 19:57:15 -0400

    login page

--- app/src/main/java/com/example/pennypig/MainActivity.java
+++ app/src/main/java/com/example/pennypig/MainActivity.java
@@ -2,25 +2,30 @@ package com.example.pennypig;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String MAINACTIVITYTAG = "MAIN ACTIVITY";
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
 
-        /* try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Log.e(MAINACTIVITYTAG, "Error in Thread.sleep");
-        }*/
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
