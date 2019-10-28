f95954621fb38c636812a2195719e072eb9b72ff
Ram Keerthy Athinarayanan <rkeerthya@gmail.com>
2019-10-28 16:37:57 -0400

    splash screen

--- app/src/main/java/com/example/pennypig/MainActivity.java
+++ app/src/main/java/com/example/pennypig/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.pennypig;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String MAINACTIVITYTAG = "MAIN ACTIVITY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
+        /* try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.e(MAINACTIVITYTAG, "Error in Thread.sleep");
+        }*/
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
