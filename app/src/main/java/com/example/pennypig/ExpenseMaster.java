9665f26e6d6b4c246fca7162cfe19fcb763765e8
ritukalsy <rkalsy@gmail.com>
2019-11-18 15:52:53 -0500

    income generated

--- app/src/main/java/com/example/pennypig/ExpenseMaster.java
+++ app/src/main/java/com/example/pennypig/ExpenseMaster.java
@@ -2,8 +2,10 @@ package com.example.pennypig;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class ExpenseMaster extends AppCompatActivity {
@@ -18,5 +20,14 @@ public class ExpenseMaster extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_master);
+
+        Button button = findViewById(R.id.buttonIncomePage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExpenseMaster.this,Income.class);
+                startActivity(intent);
+            }
+        });
     }
 }
